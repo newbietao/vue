8e75fb383f767f5607b71d862c0bd4e87bfd3cf6
newbietao <1010252139@qq.com>
2017-05-01 10:26:32 +0800

    vue 使用bootstrap

--- webpack/webpack_simple/src/components/Welcome.vue
+++ webpack/webpack_simple/src/components/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h3>welcome to vue!</h3>
-        <button @click="add" class="btn btn-info">点我加一</button>
+        <button @click="add" class="btn btn-info">点我加一 <span class="glyphicon glyphicon-plus"></span></button>
         <div>{{count}}</div>
     </div>
 </template>
