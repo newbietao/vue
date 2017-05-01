ba3d8af1cfda733b8958e1bc7969e5266aa3f321
newbietao <1010252139@qq.com>
2017-05-01 09:03:26 +0800

    vue 脚手架工具实现组件化

---
+++ webpack/webpack_simple/src/components/Welcome.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h3>welcome to vue!</h3>
+        <button @click="add">点我加一</button>
+        <div>{{count}}</div>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                count:0
+            }
+        },
+        methods:{
+            add(){
+                this.count++;
+            }
+        }
+    }
+</script>
