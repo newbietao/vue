4d6fd225cc672a3e13f000979bd82d82e62ffee5
newbietao <1010252139@qq.com>
2017-05-01 15:13:56 +0800

    vue 项目引入bootstrap的另一种方法--直接在index.html中引入

--- webpack/vue_router/src/components/App.vue
+++ webpack/vue_router/src/components/App.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
        <h3>router</h3>
-        <router-link to="/home">home</router-link>
-        <router-link to="/user">user</router-link>
+        <button class="btn btn-warning">11</button>
+        <router-link to="/home"><button class="btn btn-info">home</button></router-link>
+        <router-link to="/user"><button class="btn btn-success">user</button></router-link>
         <router-view></router-view>
     </div>
 </template>
