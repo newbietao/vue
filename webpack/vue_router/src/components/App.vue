c58b03fb62fb7986c0ced78ca9c162161bcb4f51
newbietao <1010252139@qq.com>
2017-05-01 15:20:50 +0800

    vue 路由过度效果

--- webpack/vue_router/src/components/App.vue
+++ webpack/vue_router/src/components/App.vue
@@ -4,7 +4,9 @@
         <button class="btn btn-warning">11</button>
         <router-link to="/home"><button class="btn btn-info">home</button></router-link>
         <router-link to="/user"><button class="btn btn-success">user</button></router-link>
-        <router-view></router-view>
+        <transition name="fade" mode="out-in">
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 <script>
@@ -20,3 +22,12 @@
         }
     }
 </script>
+<style scoped >
+    .fade-enter-active,.fade-leave-active{
+        transition: all 0.5s;
+    }
+    .fade-enter , .fade-leave-active{
+        transform: translateX(10px);
+        opacity: 0;
+    }
+</style>
