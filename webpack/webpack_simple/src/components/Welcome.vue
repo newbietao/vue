07d411d30c0ee0181ba0384ef884ed268ccff1f1
newbietao <1010252139@qq.com>
2017-05-01 09:28:40 +0800

    vue 脚手架工具引入bootstrap

--- webpack/webpack_simple/src/components/Welcome.vue
+++ webpack/webpack_simple/src/components/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h3>welcome to vue!</h3>
-        <button @click="add">点我加一</button>
+        <button @click="add" class="btn btn-info">点我加一</button>
         <div>{{count}}</div>
     </div>
 </template>
