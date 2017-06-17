3c6acc16bb99871e574cbf88ce24a54d76747a02
newbietao <1010252139@qq.com>
2017-06-17 15:35:54 +0800

    render函数v-model

--- render/src/App.vue
+++ render/src/App.vue
@@ -1,23 +1,44 @@
 <template>
   <div id="app1">
       <my-heads :level="level" title="hello world">hello vue</my-heads>
+      {{value}}
   </div>
 </template>
 
 <script>
    var myHeads = {
         name:"myHeads",
+       data(){
+            return {
+                value:""
+            }
+       },
         props:{
             level:"",
-            title:""
+            title:"",
         },
-        render:function(h){
-            return h(
-                "h"+this.level,
-                this.$slots.default
+       render: function (createElement) {
+           var self = this
+           return createElement('div', {
 
-            );
-        }
+           },[
+                   createElement('input', {
+                       domProps: {
+                           value: self.value
+                       },
+                       on: {
+                           input: function (event) {
+                               self.value = event.target.value
+                           }
+                       }
+                   }),
+                this.value
+           ]
+
+           )
+
+
+       }
     }
 export default {
   name: 'app',
@@ -25,6 +46,7 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App',
         level:1,
+        value:""
     }
 
   },
