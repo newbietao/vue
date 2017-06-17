777625b3e27cbe87c088e8b9002b446405d1496a
newbietao <1010252139@qq.com>
2017-06-17 14:44:15 +0800

    render函数的初次尝试

---
+++ render/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app1">
+      <my-heads :level="level" title="hello world">hello vue</my-heads>
+  </div>
+</template>
+
+<script>
+   var myHeads = {
+        name:"myHeads",
+        props:{
+            level:"",
+            title:""
+        },
+        render:function(h){
+            return h(
+                "h"+this.level,
+                this.$slots.default
+
+            );
+        }
+    }
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+        level:1,
+    }
+
+  },
+    components:{
+        myHeads
+    }
+}
+</script>
+
+<style>
+
+</style>
